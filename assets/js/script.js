0661b87087dffc24cc7b932bc6379067c95bc682
TYom4ikk <Zhakubekov20032006@gmail.com>
2023-11-07 17:49:22 +0500

    Creating new Background, new Font Color and Backend logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,11 +30,11 @@ myLink[1].addEventListener("click", function (event) {
   console.log("Ссылка была кликнута!");
 });
 
-let Email = document.getElementById("email");
-let Password = document.getElementById("password");
-let conifurm_password = document.getElementById("conifurm_password");
-let Name = document.getElementById("name");
-let Surname = document.getElementById("surname");
+let Email = document.getElementById("Register_email");
+let Password = document.getElementById("Register_password");
+let conifurm_password = document.getElementById("Register_conifurm_password");
+let Name = document.getElementById("Register_name");
+let Surname = document.getElementById("Register_surname");
 
 var Masuk = document.getElementsByClassName("button");
 const DataArray = [];
@@ -48,6 +48,13 @@ Masuk[1].addEventListener("click", function (event) {
     Name,
     Surname,
   });
-  console.log(DataArray[0]);
+  
+  console.log(
+    DataArray[0].Email.value,
+    DataArray[0].Password.value,
+    DataArray[0].conifurm_password.value,
+    DataArray[0].Name.value,
+    DataArray[0].Surname.value,
+  );
   alert("Данные записаны!");
 });
